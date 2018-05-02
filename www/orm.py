83e88f3b82d961a10fac138d2e6d3eebcf394dd1
jason <335326056@qq.com>
2018-05-02 18:00:16 +0800

    add models class

--- www/orm.py
+++ www/orm.py
@@ -31,7 +31,7 @@ def select(sql,args,size=None):
     global __pool
     with (yield from __pool) as conn:
         cur = yield from conn.cursor(aiomysql.DictCursor)
-        yield from cur.exec(sql.replace('?','%s'), args or ())
+        yield from cur.execute(sql.replace('?','%s'), args or ())
         if size:
             rs = yield from cur.fetchmany(size)
         else:
