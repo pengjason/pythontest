9066569a93ff575720429b1a34c0f7b9d71f0364
jason <335326056@qq.com>
2018-05-02 23:07:40 +0800

    add sql script

--- www/models.py
+++ www/models.py
@@ -27,3 +27,17 @@ class Blog(Model):
     summary = StringField(ddl='varchar(200)')
     content = TextField
     created_at = FloatField(default=time.time)
+    
+class Comment(Model):
+    __table__= 'comments'
+    
+    id = StringField(primary_key=True, default=next_id, ddl='varchar(50)')
+    blog_id = StringField(ddl='varchar(50)')
+    user_id = StringField(ddl='varchar(50)')
+    user_name = StringField(ddl='varchar(50)')
+    user_image = StringField(ddl='varchar(500)')
+    content = TextField
+    created_at = FloatField(default=time.time)
+    
+    
+    
