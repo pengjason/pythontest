f621e7e0b7e9bd0e1c873148c9f633e4571e1bac
jason <335326056@qq.com>
2018-05-08 19:31:57 +0800

    add init_jinja2 in app.py from office

--- www/app.py
+++ www/app.py
@@ -4,7 +4,29 @@ import asyncio,os,json,time
 from datetime import datetime
 
 from aiohttp import web
-
+from jinja2.loaders import FileSystemLoader,Environment
+from coroweb import add_routes, add_static
+def init_jinja2(app,**kw):
+    logging.info('init jinja2')
+    options = dict(
+        autoescape = kw.get('autoescape',True),
+        block_start_string = kw.get('block_start_string','{%'),
+        block_end_string = kw.get('block_end_string','%}'),
+        variable_start_string = kw.get('variable_start_string','{{'),
+        variable_end_string = kw.get('variable_end_string','}}'),
+         auto_reload = kw.get('auto_reload',True)                          
+    )
+    path = kw.get('path',None)
+    if path is None:
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)),'templates')
+    logging.info('set jinja2 template path: %s' % path)
+    env = Enviroment(loader=FileSystemLoader(path),**options)
+    filters = kw.get('filters',None)
+    if filters is not None:
+        for name, f in filters.items():
+            env.filters[name] = f
+    app['__templating__'] = env
+    
 def index(request):
     return web.Response(body=b'<h1> Awesome </h1>',content_type='text/html')
 #       return web.Response(body=u'<h1> Awesome 你好</h1>'.encode(encoding='gbk'),content_type='text/html')
