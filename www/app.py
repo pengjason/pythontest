dd8d27dc28c3efa1c79b79a63441044b691a6514
jason <335326056@qq.com>
2018-06-01 19:12:52 +0800

    finish day 10 job from office

--- www/app.py
+++ www/app.py
@@ -18,7 +18,7 @@ from config import configs
 import orm
 from coroweb import add_routes, add_static
 
-# from handlers import cookie2user, COOKIE_NAME
+from handlers import cookie2user, COOKIE_NAME
 
 def init_jinja2(app,**kw):
     logging.info('init jinja2')
@@ -42,30 +42,53 @@ def init_jinja2(app,**kw):
             env.filters[name] = f
     app['__templating__'] = env
 
-async def logger_factory(app,handler):
-    async def logger(request):
-        logging.info('Request: %s %s' % (request.method,request.path))
-         # await asyncio.sleep(0.3)
-        return (await handler(request))
+@asyncio.coroutine
+def logger_factory(app, handler):
+    @asyncio.coroutine
+    def logger(request):
+        logging.info('Request: %s %s' % (request.method, request.path))
+        # yield from asyncio.sleep(0.3)
+        return (yield from handler(request))
     return logger
 
-async def data_factory(app,handler):
-    async def parse_data(request):
+@asyncio.coroutine
+def auth_factory(app, handler):
+    @asyncio.coroutine
+    def auth(request):
+        logging.info('check user: %s %s' % (request.method, request.path))
+        request.__user__ = None
+        cookie_str = request.cookies.get(COOKIE_NAME)
+        if cookie_str:
+            user = yield from cookie2user(cookie_str)
+            if user:
+                logging.info('set current user: %s' % user.email)
+                request.__user__ = user
+        if request.path.startswith('/manage/') and (request.__user__ is None or not request.__user__.admin):
+            return web.HTTPFound('/signin')
+        return (yield from handler(request))
+    return auth
+
+@asyncio.coroutine
+def data_factory(app, handler):
+    @asyncio.coroutine
+    def parse_data(request):
         if request.method == 'POST':
             if request.content_type.startswith('application/json'):
-                request.__data__ = await request.json()
+                request.__data__ = yield from request.json()
                 logging.info('request json: %s' % str(request.__data__))
             elif request.content_type.startswith('application/x-www-form-urlencoded'):
-                request.__data__ = await request.post()
+                request.__data__ = yield from request.post()
                 logging.info('request form: %s' % str(request.__data__))
-        return (await handler(request))
+        return (yield from handler(request))
     return parse_data
 
-async def response_factory(app,handler):
-    async def response(request):
+@asyncio.coroutine
+def response_factory(app, handler):
+    @asyncio.coroutine
+    def response(request):
         logging.info('Response handler...')
-        r = await handler(request)
-        if isinstance(r,web.StreamResponse):
+        r = yield from handler(request)
+        if isinstance(r, web.StreamResponse):
             return r
         if isinstance(r,bytes):
             resp = web.Response(body=r)
@@ -121,7 +144,7 @@ def datetime_filter(t):
 async def init(loop):
     await orm.create_pool(loop=loop, host='127.0.0.1', port=3306, user='root', password='peng',db='awesome')
     app = web.Application(loop=loop,middlewares=[
-        logger_factory,response_factory
+        logger_factory, auth_factory, response_factory
         ])
     init_jinja2(app,filters=dict(datetime=datetime_filter))
     add_routes(app,'handlers')
@@ -160,7 +183,7 @@ def destory_pool():
         __pool.close()
         yield from __pool.wait_closed()
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(test())
- 
-destory_pool()
+# loop = asyncio.get_event_loop()
+# loop.run_until_complete(test())
+#  
+# destory_pool()
